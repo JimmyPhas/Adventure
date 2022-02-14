d7b991f085fe99d38f56b66e6ed1a194d7a036c5
Jim Phas <JimPhas@gmail.com>
2022-02-14 08:26:06 -0500

    added everything

---
+++ models/adventure.model.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, Sequelize) => {
+    const Story = sequelize.define("story", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+    return Story;
+  };
