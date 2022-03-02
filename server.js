cf275627fc2f8d06b694a3a228c47fd6abee6265
Jim Phas <JimPhas@gmail.com>
2022-03-02 08:23:34 -0500

    formatting

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// simple route
 
+// simple route
 const db = require("./models");
-// db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to StoryTime." });
