cea243ab9c30d4b6e3020969a743ac836f814bad
Jim Phas <JimPhas@gmail.com>
2022-03-03 11:49:50 -0500

    changed and added more models

--- models/index.js
+++ models/index.js
@@ -11,8 +11,32 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     idle: dbConfig.pool.idle
   }
 });
+
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-db.stories = require("./adventure.model.js")(sequelize, Sequelize);
+
+db.stories = require("./story.model.js")(sequelize, Sequelize);
+db.events = require("./eventText.model.js")(sequelize, Sequelize);
+db.actions = require("./actionText.model.js")(sequelize, Sequelize);
+
+db.events.belongsToMany(db.stories, {
+  through: "story_events",
+  as: "story",
+  foreignKey: "event_id",
+});
+db.stories.belongsToMany(db.events, {
+  through: "story_events",
+  as: "events",
+  foreignKey: "story_id",
+});
+
+db.events.hasMany(db.actions, {
+  as: "action"
+});
+db.actions.belongsTo(db.events, {
+  foreignKey: "events_id",
+  as: "event",
+});
+
 module.exports = db;
